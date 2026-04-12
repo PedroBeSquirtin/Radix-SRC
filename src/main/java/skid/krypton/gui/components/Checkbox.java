0cce915c3b3512fc59994f22be41c03ba696b77e
PedroBeSquirtin <connorjackhughes011@gmail.com>
2026-04-12 20:11:38 +0100

    Update Checkbox.java

--- src/main/java/skid/krypton/gui/components/Checkbox.java
+++ src/main/java/skid/krypton/gui/components/Checkbox.java
@@ -1,120 +1,114 @@
 package skid.krypton.gui.components;
-
+ 
 import net.minecraft.client.gui.DrawContext;
 import skid.krypton.gui.Component;
 import skid.krypton.module.setting.Setting;
 import skid.krypton.module.setting.BooleanSetting;
-import skid.krypton.utils.Utils;
-import skid.krypton.utils.MathUtil;
-import skid.krypton.utils.RenderUtils;
+import skid.krypton.utils.*;
 import skid.krypton.utils.TextRenderer;
-
+ 
 import java.awt.*;
-
+ 
 public final class Checkbox extends Component {
+ 
+    private static final Color TEXT_COLOR  = new Color(210, 215, 225);
+    private static final Color HOVER_COLOR = new Color(255, 255, 255, 10);
+    private static final Color BOX_BORDER  = new Color(55, 62, 78);
+    private static final Color BOX_BG      = new Color(28, 33, 42);
+    private static final int   BOX_SIZE    = 13;
+ 
     private final BooleanSetting setting;
-    private float hoverAnimation;
-    private float enabledAnimation;
-    private final float CORNER_RADIUS = 3.0f;
-    private final Color TEXT_COLOR;
-    private final Color HOVER_COLOR;
-    private final Color BOX_BORDER;
-    private final Color BOX_BG;
-    private final int BOX_SIZE = 13;
-    private final float HOVER_ANIMATION_SPEED = 0.005f;
-    private final float TOGGLE_ANIMATION_SPEED = 0.002f;
-
-    public Checkbox(final ModuleButton moduleButton, final Setting setting, final int n) {
-        super(moduleButton, setting, n);
-        this.hoverAnimation = 0.0f;
-        this.enabledAnimation = 0.0f;
-        this.TEXT_COLOR = new Color(230, 230, 230);
-        this.HOVER_COLOR = new Color(255, 255, 255, 20);
-        this.BOX_BORDER = new Color(100, 100, 110);
-        this.BOX_BG = new Color(40, 40, 45);
-        this.setting = (BooleanSetting) setting;
-        float enabledAnimation;
-        if (this.setting.getValue()) {
-            enabledAnimation = 1.0f;
-        } else {
-            enabledAnimation = 0.0f;
-        }
-        this.enabledAnimation = enabledAnimation;
+    private float hoverAnim;
+    private float enabledAnim;
+ 
+    public Checkbox(ModuleButton parent, Setting setting, int offset) {
+        super(parent, setting, offset);
+        this.setting     = (BooleanSetting) setting;
+        this.hoverAnim   = 0f;
+        this.enabledAnim = this.setting.getValue() ? 1f : 0f;
     }
-
+ 
     @Override
-    public void render(final DrawContext drawContext, final int n, final int n2, final float n3) {
-        super.render(drawContext, n, n2, n3);
-        this.updateAnimations(n, n2, n3);
-        if (!this.parent.parent.dragging) {
-            drawContext.fill(this.parentX(), this.parentY() + this.parentOffset() + this.offset, this.parentX() + this.parentWidth(), this.parentY() + this.parentOffset() + this.offset + this.parentHeight(), new Color(this.HOVER_COLOR.getRed(), this.HOVER_COLOR.getGreen(), this.HOVER_COLOR.getBlue(), (int) (this.HOVER_COLOR.getAlpha() * this.hoverAnimation)).getRGB());
-        }
-        TextRenderer.drawString(this.setting.getName(), drawContext, this.parentX() + 27, this.parentY() + this.parentOffset() + this.offset + this.parentHeight() / 2 - 6, this.TEXT_COLOR.getRGB());
-        this.renderModernCheckbox(drawContext);
+    public void render(DrawContext ctx, int mx, int my, float delta) {
+        super.render(ctx, mx, my, delta);
+        updateAnimations(mx, my, delta);
+ 
+        // hover row highlight
+        if (!parent.parent.dragging && hoverAnim > 0.01f)
+            ctx.fill(parentX(), parentY() + parentOffset() + offset,
+                    parentX() + parentWidth(), parentY() + parentOffset() + offset + parentHeight(),
+                    new Color(255, 255, 255, (int)(10 * hoverAnim)).getRGB());
+ 
+        // label
+        TextRenderer.drawString(setting.getName(), ctx,
+                parentX() + 27,
+                parentY() + parentOffset() + offset + parentHeight() / 2 - 5,
+                TEXT_COLOR.getRGB());
+ 
+        renderBox(ctx);
     }
-
-    private void updateAnimations(final int n, final int n2, final float n3) {
-        final float n4 = n3 * 0.05f;
-        float n5;
-        if (this.isHovered(n, n2) && !this.parent.parent.dragging) {
-            n5 = 1.0f;
-        } else {
-            n5 = 0.0f;
-        }
-        this.hoverAnimation = (float) MathUtil.exponentialInterpolate(this.hoverAnimation, n5, 0.004999999888241291, n4);
-        float n6;
-        if (this.setting.getValue()) {
-            n6 = 1.0f;
-        } else {
-            n6 = 0.0f;
-        }
-        this.enabledAnimation = (float) MathUtil.exponentialInterpolate(this.enabledAnimation, n6, 0.0020000000949949026, n4);
-        this.enabledAnimation = (float) MathUtil.clampValue(this.enabledAnimation, 0.0, 1.0);
-    }
-
-    private void renderModernCheckbox(final DrawContext drawContext) {
-        final int n = this.parentX() + 8;
-        final int n2 = this.parentY() + this.parentOffset() + this.offset + this.parentHeight() / 2 - 6;
-        final Color mainColor = Utils.getMainColor(255, this.parent.settings.indexOf(this));
-        RenderUtils.renderRoundedQuad(drawContext.getMatrices(), this.BOX_BORDER, n, n2, n + 13, n2 + 13, 3.0, 3.0, 3.0, 3.0, 50.0);
-        RenderUtils.renderRoundedQuad(drawContext.getMatrices(), this.BOX_BG, n + 1, n2 + 1, n + 13 - 1, n2 + 13 - 1, 2.5, 2.5, 2.5, 2.5, 50.0);
-        if (this.enabledAnimation > 0.01f) {
-            final Color color = new Color(mainColor.getRed(), mainColor.getGreen(), mainColor.getBlue(), (int) (255.0f * this.enabledAnimation));
-            final float n3 = n + 2 + 9.0f * (1.0f - this.enabledAnimation) / 2.0f;
-            final float n4 = n2 + 2 + 9.0f * (1.0f - this.enabledAnimation) / 2.0f;
-            RenderUtils.renderRoundedQuad(drawContext.getMatrices(), color, n3, n4, n3 + 9.0f * this.enabledAnimation, n4 + 9.0f * this.enabledAnimation, 1.5, 1.5, 1.5, 1.5, 50.0);
-            if (this.enabledAnimation > 0.7f) {
-                RenderUtils.renderRoundedQuad(drawContext.getMatrices(), new Color(mainColor.getRed(), mainColor.getGreen(), mainColor.getBlue(), (int) (40.0f * ((this.enabledAnimation - 0.7f) * 3.33f))), n - 1, n2 - 1, n + 13 + 1, n2 + 13 + 1, 3.5, 3.5, 3.5, 3.5, 50.0);
+ 
+    private void renderBox(DrawContext ctx) {
+        final int bx = parentX() + 8;
+        final int by = parentY() + parentOffset() + offset + parentHeight() / 2 - BOX_SIZE / 2;
+        final Color accent = Utils.getMainColor(255, parent.settings.indexOf(this));
+ 
+        // border
+        RenderUtils.renderRoundedQuad(ctx.getMatrices(), BOX_BORDER,
+                bx, by, bx + BOX_SIZE, by + BOX_SIZE,
+                3.0, 3.0, 3.0, 3.0, 50.0);
+        // background
+        RenderUtils.renderRoundedQuad(ctx.getMatrices(), BOX_BG,
+                bx + 1, by + 1, bx + BOX_SIZE - 1, by + BOX_SIZE - 1,
+                2.5, 2.5, 2.5, 2.5, 50.0);
+ 
+        // fill (animated)
+        if (enabledAnim > 0.01f) {
+            final Color fill = new Color(accent.getRed(), accent.getGreen(), accent.getBlue(),
+                    (int)(255f * enabledAnim));
+            final float pad = 2f + 5f * (1f - enabledAnim) * 0.5f;
+            RenderUtils.renderRoundedQuad(ctx.getMatrices(), fill,
+                    bx + pad, by + pad,
+                    bx + BOX_SIZE - pad, by + BOX_SIZE - pad,
+                    1.5, 1.5, 1.5, 1.5, 50.0);
+            // glow
+            if (enabledAnim > 0.7f) {
+                final float gf = (enabledAnim - 0.7f) * 3.33f;
+                RenderUtils.renderRoundedQuad(ctx.getMatrices(),
+                        new Color(accent.getRed(), accent.getGreen(), accent.getBlue(), (int)(35f * gf)),
+                        bx - 1, by - 1, bx + BOX_SIZE + 1, by + BOX_SIZE + 1,
+                        3.5, 3.5, 3.5, 3.5, 50.0);
             }
         }
     }
-
-    @Override
-    public void keyPressed(final int n, final int n2, final int n3) {
-        if (this.mouseOver && this.parent.extended && n == 259) {
-            this.setting.setValue(this.setting.getDefaultValue());
-        }
-        super.keyPressed(n, n2, n3);
+ 
+    private void updateAnimations(int mx, int my, float delta) {
+        final float dt = delta * 0.05f;
+        hoverAnim   = (float) MathUtil.exponentialInterpolate(hoverAnim,
+                (isHovered(mx, my) && !parent.parent.dragging) ? 1f : 0f, 0.25, dt);
+        enabledAnim = (float) MathUtil.exponentialInterpolate(enabledAnim,
+                setting.getValue() ? 1f : 0f, 0.008, dt);
+        enabledAnim = (float) MathUtil.clampValue(enabledAnim, 0.0, 1.0);
     }
-
+ 
     @Override
-    public void mouseClicked(final double n, final double n2, final int n3) {
-        if (this.isHovered(n, n2) && n3 == 0) {
-            this.setting.toggle();
-        }
-        super.mouseClicked(n, n2, n3);
+    public void keyPressed(int key, int scan, int mods) {
+        if (mouseOver && parent.extended && key == 259)
+            setting.setValue(setting.getDefaultValue());
+        super.keyPressed(key, scan, mods);
     }
-
+ 
+    @Override
+    public void mouseClicked(double mx, double my, int btn) {
+        if (isHovered(mx, my) && btn == 0) setting.toggle();
+        super.mouseClicked(mx, my, btn);
+    }
+ 
     @Override
     public void onGuiClose() {
         super.onGuiClose();
-        this.hoverAnimation = 0.0f;
-        float enabledAnimation;
-        if (this.setting.getValue()) {
-            enabledAnimation = 1.0f;
-        } else {
-            enabledAnimation = 0.0f;
-        }
-        this.enabledAnimation = enabledAnimation;
+        hoverAnim   = 0f;
+        enabledAnim = setting.getValue() ? 1f : 0f;
     }
 }
+ 
