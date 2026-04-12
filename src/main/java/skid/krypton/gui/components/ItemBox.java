de4421f061b1f026324acfeae0c54abbb8c5b9e1
PedroBeSquirtin <connorjackhughes011@gmail.com>
2026-04-12 20:14:57 +0100

    Update ItemBox.java

--- src/main/java/skid/krypton/gui/components/ItemBox.java
+++ src/main/java/skid/krypton/gui/components/ItemBox.java
@@ -1,5 +1,5 @@
 package skid.krypton.gui.components;
-
+ 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -7,93 +7,86 @@ import net.minecraft.item.Items;
 import skid.krypton.gui.Component;
 import skid.krypton.module.setting.Setting;
 import skid.krypton.module.setting.ItemSetting;
-import skid.krypton.utils.ColorUtil;
-import skid.krypton.utils.Utils;
-import skid.krypton.utils.MathUtil;
-import skid.krypton.utils.RenderUtils;
+import skid.krypton.utils.*;
 import skid.krypton.utils.TextRenderer;
-
+ 
 import java.awt.*;
-
+ 
 public final class ItemBox extends Component {
+ 
+    private static final Color TEXT_COLOR  = new Color(210, 215, 225);
+    private static final Color HOVER_COLOR = new Color(255, 255, 255, 10);
+    private static final Color ITEM_BG     = new Color(28, 33, 43);
+    private static final Color ITEM_BORDER = new Color(52, 58, 74);
+ 
     private final ItemSetting setting;
-    private float hoverAnimation;
+    private float hoverAnim;
     private Color currentColor;
-    private final Color TEXT_COLOR;
-    private final Color HOVER_COLOR;
-    private final Color ITEM_BG;
-    private final Color ITEM_BORDER;
-    private final float CORNER_RADIUS = 4.0f;
-    private final float HOVER_ANIMATION_SPEED = 0.25f;
-
-    public ItemBox(final ModuleButton moduleButton, final Setting setting, final int n) {
-        super(moduleButton, setting, n);
-        this.hoverAnimation = 0.0f;
-        this.TEXT_COLOR = new Color(230, 230, 230);
-        this.HOVER_COLOR = new Color(255, 255, 255, 20);
-        this.ITEM_BG = new Color(30, 30, 35);
-        this.ITEM_BORDER = new Color(60, 60, 65);
+ 
+    public ItemBox(ModuleButton parent, Setting setting, int offset) {
+        super(parent, setting, offset);
         this.setting = (ItemSetting) setting;
     }
-
+ 
     @Override
     public void onUpdate() {
-        final Color mainColor = Utils.getMainColor(255, this.parent.settings.indexOf(this));
-        if (this.currentColor == null) {
-            this.currentColor = new Color(mainColor.getRed(), mainColor.getGreen(), mainColor.getBlue(), 0);
-        } else {
-            this.currentColor = new Color(mainColor.getRed(), mainColor.getGreen(), mainColor.getBlue(), this.currentColor.getAlpha());
-        }
-        if (this.currentColor.getAlpha() != 255) {
-            this.currentColor = ColorUtil.a(0.05f, 255, this.currentColor);
-        }
+        final Color mc = Utils.getMainColor(255, parent.settings.indexOf(this));
+        if (currentColor == null)
+            currentColor = new Color(mc.getRed(), mc.getGreen(), mc.getBlue(), 0);
+        else
+            currentColor = new Color(mc.getRed(), mc.getGreen(), mc.getBlue(), currentColor.getAlpha());
+        if (currentColor.getAlpha() != 255)
+            currentColor = ColorUtil.a(0.05f, 255, currentColor);
         super.onUpdate();
     }
-
+ 
     @Override
-    public void render(final DrawContext drawContext, final int n, final int n2, final float n3) {
-        super.render(drawContext, n, n2, n3);
-        this.updateAnimations(n, n2, n3);
-        if (!this.parent.parent.dragging) {
-            drawContext.fill(this.parentX(), this.parentY() + this.parentOffset() + this.offset, this.parentX() + this.parentWidth(), this.parentY() + this.parentOffset() + this.offset + this.parentHeight(), new Color(this.HOVER_COLOR.getRed(), this.HOVER_COLOR.getGreen(), this.HOVER_COLOR.getBlue(), (int) (this.HOVER_COLOR.getAlpha() * this.hoverAnimation)).getRGB());
-        }
-        final int n4 = this.parentX() + 5;
-        final int n5 = this.parentY() + this.parentOffset() + this.offset + this.parentHeight() / 2;
-        TextRenderer.drawString(String.valueOf(this.setting.getName()), drawContext, n4, n5 - 8, this.TEXT_COLOR.getRGB());
-        final int n6 = n4 + TextRenderer.getWidth(this.setting.getName() + ": ") + 5;
-        final int n7 = n5 - 11;
-        RenderUtils.renderRoundedQuad(drawContext.getMatrices(), this.ITEM_BORDER, n6, n7, n6 + 22, n7 + 22, 4.0, 4.0, 4.0, 4.0, 50.0);
-        RenderUtils.renderRoundedQuad(drawContext.getMatrices(), this.ITEM_BG, n6 + 1, n7 + 1, n6 + 22 - 1, n7 + 22 - 1, 3.5, 3.5, 3.5, 3.5, 50.0);
-        final Item a = this.setting.getItem();
-        if (a != null && a != Items.AIR) {
-            drawContext.drawItem(new ItemStack(a), n6 + 3, n7 + 3);
-        } else {
-            TextRenderer.drawCenteredString("?", drawContext, n6 + 11 - 1, n7 + 4, new Color(150, 150, 150, 200).getRGB());
-        }
+    public void render(DrawContext ctx, int mx, int my, float delta) {
+        super.render(ctx, mx, my, delta);
+        updateAnimations(mx, my, delta);
+ 
+        if (!parent.parent.dragging && hoverAnim > 0.01f)
+            ctx.fill(parentX(), parentY() + parentOffset() + offset,
+                    parentX() + parentWidth(), parentY() + parentOffset() + offset + parentHeight(),
+                    new Color(255, 255, 255, (int)(10 * hoverAnim)).getRGB());
+ 
+        final int labelX = parentX() + 6;
+        final int labelY = parentY() + parentOffset() + offset + parentHeight() / 2 - 5;
+        TextRenderer.drawString(setting.getName(), ctx, labelX, labelY, TEXT_COLOR.getRGB());
+ 
+        // item box (right side)
+        final int boxX = labelX + TextRenderer.getWidth(setting.getName() + ": ") + 6;
+        final int boxY = labelY - 8;
+        RenderUtils.renderRoundedQuad(ctx.getMatrices(), ITEM_BORDER,
+                boxX, boxY, boxX + 22, boxY + 22, 4.0, 4.0, 4.0, 4.0, 50.0);
+        RenderUtils.renderRoundedQuad(ctx.getMatrices(), ITEM_BG,
+                boxX + 1, boxY + 1, boxX + 21, boxY + 21, 3.5, 3.5, 3.5, 3.5, 50.0);
+ 
+        final Item item = setting.getItem();
+        if (item != null && item != Items.AIR)
+            ctx.drawItem(new ItemStack(item), boxX + 3, boxY + 3);
+        else
+            TextRenderer.drawCenteredString("?", ctx, boxX + 11, boxY + 4,
+                    new Color(140, 148, 165, 200).getRGB());
     }
-
-    private void updateAnimations(final int n, final int n2, final float n3) {
-        float n4;
-        if (this.isHovered(n, n2) && !this.parent.parent.dragging) {
-            n4 = 1.0f;
-        } else {
-            n4 = 0.0f;
-        }
-        this.hoverAnimation = (float) MathUtil.exponentialInterpolate(this.hoverAnimation, n4, 0.25, n3 * 0.05f);
+ 
+    private void updateAnimations(int mx, int my, float delta) {
+        hoverAnim = (float) MathUtil.exponentialInterpolate(hoverAnim,
+                (isHovered(mx, my) && !parent.parent.dragging) ? 1f : 0f,
+                0.25, delta * 0.05f);
     }
-
+ 
     @Override
-    public void mouseClicked(final double n, final double n2, final int n3) {
-        if (this.isHovered(n, n2) && n3 == 0) {
-            this.mc.setScreen(new ItemFilter(this, this.setting));
-        }
-        super.mouseClicked(n, n2, n3);
+    public void mouseClicked(double mx, double my, int btn) {
+        if (isHovered(mx, my) && btn == 0)
+            mc.setScreen(new ItemFilter(this, setting));
+        super.mouseClicked(mx, my, btn);
     }
-
+ 
     @Override
     public void onGuiClose() {
-        this.currentColor = null;
-        this.hoverAnimation = 0.0f;
+        currentColor = null;
+        hoverAnim    = 0f;
         super.onGuiClose();
     }
 }
