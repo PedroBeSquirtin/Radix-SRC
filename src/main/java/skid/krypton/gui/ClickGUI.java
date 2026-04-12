3c0291f981f9203d975488cef7478c6d01cc3a44
PedroBeSquirtin <connorjackhughes011@gmail.com>
2026-04-12 20:10:21 +0100

    Update ClickGUI.java

--- src/main/java/skid/krypton/gui/ClickGUI.java
+++ src/main/java/skid/krypton/gui/ClickGUI.java
@@ -1,5 +1,5 @@
 package skid.krypton.gui;
-
+ 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
@@ -9,12 +9,12 @@ import skid.krypton.module.Category;
 import skid.krypton.utils.ColorUtil;
 import skid.krypton.utils.RenderUtils;
 import skid.krypton.utils.TextRenderer;
-
+ 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+ 
 public final class ClickGUI extends Screen {
     public List<CategoryWindow> windows;
     public Color currentColor;
@@ -22,162 +22,143 @@ public final class ClickGUI extends Screen {
     private int tooltipX;
     private int tooltipY;
     private final Color DESCRIPTION_BG;
-
+ 
+    // Panel dimensions matching the screenshot style
+    public static final int PANEL_WIDTH  = 160;
+    public static final int HEADER_HEIGHT = 36;
+    public static final int ROW_HEIGHT   = 32;
+ 
     public ClickGUI() {
         super(Text.empty());
         this.windows = new ArrayList<>();
         this.tooltipText = null;
-        this.DESCRIPTION_BG = new Color(25, 30, 38, 240);
-        int n = 20;
+        this.DESCRIPTION_BG = new Color(20, 24, 30, 245);
+ 
+        int x = 18;
         final Category[] values = Category.values();
         for (Category value : values) {
-            this.windows.add(new CategoryWindow(n, 20, 100, 25, value, this));
-            n += 110;
+            this.windows.add(new CategoryWindow(x, 18, PANEL_WIDTH, HEADER_HEIGHT, value, this));
+            x += PANEL_WIDTH + 10;
         }
     }
-
+ 
     public boolean isDraggingAlready() {
         for (CategoryWindow window : this.windows) {
-            if (window.dragging) {
-                return true;
-            }
+            if (window.dragging) return true;
         }
         return false;
     }
-
+ 
     public void setTooltip(final CharSequence tooltipText, final int tooltipX, final int tooltipY) {
         this.tooltipText = tooltipText;
-        this.tooltipX = tooltipX;
-        this.tooltipY = tooltipY;
+        this.tooltipX    = tooltipX;
+        this.tooltipY    = tooltipY;
     }
-
+ 
     public void setInitialFocus() {
-        if (this.client == null) {
-            return;
-        }
+        if (this.client == null) return;
         super.setInitialFocus();
     }
-
+ 
+    @Override
     public void render(final DrawContext drawContext, final int n, final int n2, final float n3) {
-        if (Krypton.mc.currentScreen == this) {
-            if (Krypton.INSTANCE.screen != null) {
-                Krypton.INSTANCE.screen.render(drawContext, 0, 0, n3);
-            }
-            if (this.currentColor == null) {
-                this.currentColor = new Color(0, 0, 0, 0);
-            } else {
-                this.currentColor = new Color(0, 0, 0, this.currentColor.getAlpha());
-            }
-            final int alpha = this.currentColor.getAlpha();
-            int n4;
-            if (skid.krypton.module.modules.client.Krypton.renderBackground.getValue()) {
-                n4 = 160;
-            } else {
-                n4 = 0;
-            }
-            if (alpha != n4) {
-                int n5;
-                if (skid.krypton.module.modules.client.Krypton.renderBackground.getValue()) {
-                    n5 = 160;
-                } else {
-                    n5 = 0;
-                }
-                this.currentColor = ColorUtil.a(0.05f, n5, this.currentColor);
-            }
-            if (Krypton.mc.currentScreen instanceof ClickGUI) {
-                drawContext.fill(0, 0, Krypton.mc.getWindow().getWidth(), Krypton.mc.getWindow().getHeight(), this.currentColor.getRGB());
-            }
-            RenderUtils.unscaledProjection();
-            final int n6 = n * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-            final int n7 = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-            super.render(drawContext, n6, n7, n3);
-            for (final CategoryWindow next : this.windows) {
-                next.render(drawContext, n6, n7, n3);
-                next.updatePosition(n6, n7, n3);
-            }
-            if (this.tooltipText != null) {
-                this.renderTooltip(drawContext, this.tooltipText, this.tooltipX, this.tooltipY);
-                this.tooltipText = null;
-            }
-            RenderUtils.scaledProjection();
+        if (Krypton.mc.currentScreen != this) return;
+ 
+        if (Krypton.INSTANCE.screen != null)
+            Krypton.INSTANCE.screen.render(drawContext, 0, 0, n3);
+ 
+        if (this.currentColor == null) {
+            this.currentColor = new Color(0, 0, 0, 0);
+        } else {
+            this.currentColor = new Color(0, 0, 0, this.currentColor.getAlpha());
         }
+ 
+        final int targetAlpha = skid.krypton.module.modules.client.Krypton.renderBackground.getValue() ? 150 : 0;
+        if (this.currentColor.getAlpha() != targetAlpha)
+            this.currentColor = ColorUtil.a(0.05f, targetAlpha, this.currentColor);
+ 
+        if (Krypton.mc.currentScreen instanceof ClickGUI)
+            drawContext.fill(0, 0, Krypton.mc.getWindow().getWidth(), Krypton.mc.getWindow().getHeight(), this.currentColor.getRGB());
+ 
+        RenderUtils.unscaledProjection();
+        final int scaledX = n  * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+        final int scaledY = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+ 
+        super.render(drawContext, scaledX, scaledY, n3);
+ 
+        for (final CategoryWindow next : this.windows) {
+            next.render(drawContext, scaledX, scaledY, n3);
+            next.updatePosition(scaledX, scaledY, n3);
+        }
+ 
+        if (this.tooltipText != null) {
+            this.renderTooltip(drawContext, this.tooltipText, this.tooltipX, this.tooltipY);
+            this.tooltipText = null;
+        }
+ 
+        RenderUtils.scaledProjection();
     }
-
+ 
+    @Override
     public boolean keyPressed(final int n, final int n2, final int n3) {
-        final Iterator<CategoryWindow> iterator = this.windows.iterator();
-        while (iterator.hasNext()) {
-            iterator.next().keyPressed(n, n2, n3);
-        }
+        for (CategoryWindow w : this.windows) w.keyPressed(n, n2, n3);
         return super.keyPressed(n, n2, n3);
     }
-
+ 
+    @Override
     public boolean mouseClicked(final double n, final double n2, final int n3) {
-        final double n4 = n * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-        final double n5 = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-        final Iterator<CategoryWindow> iterator = this.windows.iterator();
-        while (iterator.hasNext()) {
-            iterator.next().mouseClicked(n4, n5, n3);
-        }
-        return super.mouseClicked(n4, n5, n3);
+        final double sx = n  * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+        final double sy = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+        for (CategoryWindow w : this.windows) w.mouseClicked(sx, sy, n3);
+        return super.mouseClicked(sx, sy, n3);
     }
-
+ 
+    @Override
     public boolean mouseDragged(final double n, final double n2, final int n3, final double n4, final double n5) {
-        final double n6 = n * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-        final double n7 = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-        final Iterator<CategoryWindow> iterator = this.windows.iterator();
-        while (iterator.hasNext()) {
-            iterator.next().mouseDragged(n6, n7, n3, n4, n5);
-        }
-        return super.mouseDragged(n6, n7, n3, n4, n5);
+        final double sx = n  * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+        final double sy = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+        for (CategoryWindow w : this.windows) w.mouseDragged(sx, sy, n3, n4, n5);
+        return super.mouseDragged(sx, sy, n3, n4, n5);
     }
-
+ 
+    @Override
     public boolean mouseScrolled(final double n, final double n2, final double n3, final double n4) {
-        final double n5 = n2 * MinecraftClient.getInstance().getWindow().getScaleFactor();
-        final Iterator<CategoryWindow> iterator = this.windows.iterator();
-        while (iterator.hasNext()) {
-            iterator.next().mouseScrolled(n, n5, n3, n4);
-        }
-        return super.mouseScrolled(n, n5, n3, n4);
+        final double sy = n2 * MinecraftClient.getInstance().getWindow().getScaleFactor();
+        for (CategoryWindow w : this.windows) w.mouseScrolled(n, sy, n3, n4);
+        return super.mouseScrolled(n, sy, n3, n4);
     }
-
-    public boolean shouldPause() {
-        return false;
-    }
-
+ 
+    @Override
+    public boolean shouldPause() { return false; }
+ 
+    @Override
     public void close() {
         Krypton.INSTANCE.getModuleManager().getModuleByClass(skid.krypton.module.modules.client.Krypton.class).setEnabled(false);
         this.onGuiClose();
     }
-
+ 
     public void onGuiClose() {
         Krypton.mc.setScreenAndRender(Krypton.INSTANCE.screen);
         this.currentColor = null;
-        final Iterator<CategoryWindow> iterator = this.windows.iterator();
-        while (iterator.hasNext()) {
-            iterator.next().onGuiClose();
-        }
+        for (CategoryWindow w : this.windows) w.onGuiClose();
     }
-
+ 
+    @Override
     public boolean mouseReleased(final double n, final double n2, final int n3) {
-        final double n4 = n * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-        final double n5 = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
-        final Iterator<CategoryWindow> iterator = this.windows.iterator();
-        while (iterator.hasNext()) {
-            iterator.next().mouseReleased(n4, n5, n3);
-        }
-        return super.mouseReleased(n4, n5, n3);
+        final double sx = n  * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+        final double sy = n2 * (int) MinecraftClient.getInstance().getWindow().getScaleFactor();
+        for (CategoryWindow w : this.windows) w.mouseReleased(sx, sy, n3);
+        return super.mouseReleased(sx, sy, n3);
     }
-
-    private void renderTooltip(final DrawContext drawContext, final CharSequence charSequence, int n, final int n2) {
-        if (charSequence == null || charSequence.length() == 0) {
-            return;
-        }
-        final int a = TextRenderer.getWidth(charSequence);
-        final int framebufferWidth = Krypton.mc.getWindow().getFramebufferWidth();
-        if (n + a + 10 > framebufferWidth) {
-            n = framebufferWidth - a - 10;
-        }
-        RenderUtils.renderRoundedQuad(drawContext.getMatrices(), this.DESCRIPTION_BG, n - 5, n2 - 5, n + a + 5, n2 + 15, 4.0, 4.0, 4.0, 4.0, 50.0);
-        TextRenderer.drawString(charSequence, drawContext, n, n2, new Color(220, 220, 225).getRGB());
+ 
+    private void renderTooltip(final DrawContext drawContext, final CharSequence text, int x, final int y) {
+        if (text == null || text.length() == 0) return;
+        final int tw = TextRenderer.getWidth(text);
+        final int fw = Krypton.mc.getWindow().getFramebufferWidth();
+        if (x + tw + 10 > fw) x = fw - tw - 10;
+        RenderUtils.renderRoundedQuad(drawContext.getMatrices(), this.DESCRIPTION_BG,
+                x - 6, y - 5, x + tw + 6, y + 16,
+                4.0, 4.0, 4.0, 4.0, 50.0);
+        TextRenderer.drawString(text, drawContext, x, y, new Color(220, 225, 235).getRGB());
     }
 }
